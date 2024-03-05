2cb5dca06d119722b193a36f63a82126e2517585
Danila Krivorotov <krivorotov.da@phystech.edu>
2024-03-05 22:21:10 +0300

    scaffnew impl, fast, but not logical

--- source/optimization/federative/scaffnew.py
+++ source/optimization/federative/scaffnew.py
@@ -6,48 +6,53 @@ class Scaffnew(BaseFederatedOptimizer):
   def __init__(
     self,
     batch_size: int = 128,
-    epochs: int = 8,
     eta: float = 1e-3,
-    proba: float = 0.01
+    epochs: int = 64,
+    proba: float = 0.01,
+
   ):
     self.batch_size: int = batch_size
-    self.epochs: int = epochs
     self.eta: float = eta
+    self.epochs = epochs
+    self.communication_rounds: int = int(epochs * proba)
     self.proba: float = proba
 
   def play_round(self, model: Model):
     if model.server.other.get("control", None) is None:
       Scaffnew._init_controls(model)
 
-    for epoch in range(self.epochs):
-      model.server.other["skip"] = np.random.choice([True, False], size=1, p=[1-self.proba, self.proba])
-      m, clients_weights, _ = model.clients_update(self.client_update)
 
+    commucations = np.random.choice(
+      np.arange(0, self.epochs), size=self.communication_rounds
+    )
+    prev = 0
+    # instead of loop use max_skip_rounds with proba parameter
+    for skip_epochs in commucations:
+      model.server.other["skip_epochs"] = skip_epochs - prev + 1
+
+      m, clients_weights, _ = model.clients_update(self.client_update)
       model.server.function.update(
         (-1) * (model.server.function.weights() - clients_weights.sum(axis=0) / m)
       )
   
   def client_update(self, server: Model.Agent, client: Model.Agent):
-    if server.other["skip"]:
+    weights_cap: np.ndarray = client.function.weights()
+    weights: np.ndarray = server.function.weights()
+
+    client.other["control"] = client.other["control"] + \
+      (self.proba / self.eta) * (weights - weights_cap)
+    client.function.update(
+      (-1) * (weights_cap - weights)
+    )
+
+    for _ in range(server.other["skip_epochs"]):
       client.function(X=client.X, y=client.y)
 
       client.function.update(
         (-1) * self.eta * (client.function.grad() - client.other["control"])
       )
-      return client
-    
-    weights = server.function.weights()
-    weights_cap: np.ndarray = client.function.weights()
-    client.other["control"] = client.other["control"] + \
-      (self.proba / self.eta) * (weights - weights_cap)
-    
-    client.function.update(
-      (-1) * self.eta * (weights_cap - weights)
-    )
 
     return client
-    
-  
   
   @staticmethod
   def _init_controls(model: Model):
